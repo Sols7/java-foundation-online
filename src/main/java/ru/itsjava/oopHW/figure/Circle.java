55f7c78c8bb79e446932c9f6086c52ccb7c7f671
Sols7 <serg-lex@yandex.ru>
2021-11-13 00:17:56 +0300

    add package statics and figure

---
+++ src/main/java/ru/itsjava/oopHW/figure/Circle.java
@@ -0,0 +1,10 @@
+package ru.itsjava.oopHW.figure;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Circle extends Figure{
+    private int radius;
+    private final static double PI = 3.141592654;
+
+}
