0c0c5993204235c92ebbfa7a0bdd525d41621ed5
Sols7 <serg-lex@yandex.ru>
2021-11-20 22:08:51 +0300

    changed class Figure

--- src/main/java/ru/itsjava/oopHW/figure/Figure.java
+++ src/main/java/ru/itsjava/oopHW/figure/Figure.java
@@ -3,10 +3,6 @@ package ru.itsjava.oopHW.figure;
 public abstract class Figure {
     final static double PI = 3.141592654;
 
-//    public double area() {
-//        return -1;
-//    }
-
     abstract double area();
 
     public double perimeter() {
