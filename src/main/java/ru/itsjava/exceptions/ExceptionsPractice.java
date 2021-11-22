5d59c2fc2cd2d33b4c0c894528d9af0ddc6d2ee3
Sols7 <serg-lex@yandex.ru>
2021-11-22 20:34:38 +0300

    add package exception

---
+++ src/main/java/ru/itsjava/exceptions/ExceptionsPractice.java
@@ -0,0 +1,48 @@
+package ru.itsjava.exceptions;
+
+public class ExceptionsPractice {
+
+    public static void main(String[] args) {
+
+//        throw new RuntimeException("My runtime exception");
+
+//        try {
+//            throw new Exception();
+//        } catch (Exception e) {
+//            System.out.println("!!!!! Exception. Be careful. !!!!!");
+////            e.printStackTrace();
+//        }
+
+//        try {
+//            System.out.println("Begin");
+////            throw new Exception();
+//        } catch (Exception e) {
+//            System.out.println("CATCH");
+////            e.printStackTrace();
+//        } finally {
+//            System.out.println("FINALLY");
+//        }
+//
+//        System.out.println("END");
+
+//        try {
+//            throw new RuntimeException();
+//        } catch (RuntimeException runtimeException){
+//            System.out.println("Runtime Exception");
+//        } catch (Throwable throwable){
+//            System.out.println("Throwable");
+//        }
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("calculator.division(5,3) = " + calculator.division(5, 3));
+
+        try{
+        System.out.println("calculator.division(5,0) = " + calculator.division(5, 0));}
+        catch (CalculatorException ce){
+            System.out.println("Вы ввели что-то не то");
+            ce.printStackTrace();
+        }
+
+    }
+}
