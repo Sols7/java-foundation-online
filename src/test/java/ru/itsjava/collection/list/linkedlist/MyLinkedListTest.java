49adfa7ebe96bf3098b5fc98920373b1161bfeb3
Sols7 <serg-lex@yandex.ru>
2021-12-08 00:38:08 +0300

    changed class MyLinkedListTest

--- src/test/java/ru/itsjava/collection/list/linkedlist/MyLinkedListTest.java
+++ src/test/java/ru/itsjava/collection/list/linkedlist/MyLinkedListTest.java
@@ -45,4 +45,66 @@ public class MyLinkedListTest {
         assertTrue(list.contains("Строка_1"));
     }
 
+    @DisplayName(" корректно выполнять метод remove")
+    @Test
+    public void shouldHaveCorrectMethodRemove() {
+        list.add("Строка_1");
+        assertEquals(1, list.size());
+        list.remove("Строка_1");
+        assertEquals(0, list.size());
+        assertFalse(list.contains("Строка_1"));
+    }
+
+    @DisplayName(" корректно выполнять метод clear")
+    @Test
+    public void shouldHaveCorrectMethodClear() {
+        list.add("Строка_1");
+        list.add("Строка_2");
+        assertFalse(list.isEmpty());
+        list.clear();
+        assertTrue(list.isEmpty());
+    }
+
+    @DisplayName(" корректно выполнять метод get")
+    @Test
+    public void shouldHaveCorrectMethodGet() {
+        list.add("Строка_1");
+        assertEquals("Строка_1", list.get(0));
+    }
+
+    @DisplayName(" корректно выполнять метод set")
+    @Test
+    public void shouldHaveCorrectMethodSet() {
+        list.add("Строка_1");
+        assertEquals("Строка_SET", list.set(0, "Строка_SET"));
+    }
+
+    @DisplayName(" корректно выполнять метод remove по индексу")
+    @Test
+    public void shouldHaveCorrectMethodRemoveIndex() {
+        list.add("Строка_1");
+        list.add("Строка_2");
+        list.add("Строка_3");
+        list.remove(1);
+        assertFalse(list.contains("Строка_2"));
+        assertEquals(2, list.size());
+    }
+
+    @DisplayName(" корректно выполнять метод indexOf")
+    @Test
+    public void shouldHaveCorrectMethodIndexOf() {
+        list.add("Строка_1");
+        list.add("Строка_2");
+        list.add("Строка_3");
+        assertEquals(0, list.indexOf("Строка_1"));
+    }
+
+    @DisplayName(" корректно выполнять метод lastIndexOf")
+    @Test
+    public void shouldHaveCorrectMethodLastIndexOf() {
+        list.add("Строка_1");
+        list.add("Строка_1");
+        list.add("Строка_3");
+        assertEquals(1, list.lastIndexOf("Строка_1"));
+    }
 }
