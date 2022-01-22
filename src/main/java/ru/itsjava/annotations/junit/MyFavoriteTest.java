6f0bf8f0a0c0faf8ca658638b387e47436051ec1
Sols7 <serg-lex@yandex.ru>
2022-01-23 00:46:47 +0300

    changed package junit

--- src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
+++ src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
@@ -2,12 +2,27 @@ package ru.itsjava.annotations.junit;
 
 public class MyFavoriteTest {
 
+    @BeforeAll
+    public void beforeAllMethod(){
+        System.out.println("BEFORE ALL");
+    }
+
+    @AfterAll
+    public void AfterAllMethod(){
+        System.out.println("AFTER ALL");
+    }
+
+    @Before
+    public void beforeMethod(){}
+
+    @DisplayName
     @Test
     public void test1(){}
 
     @Test
     public void test2(){}
 
+    @DisplayName
     @Test
     public void test3(){
         throw new RuntimeException();
---
+++ src/main/java/ru/itsjava/annotations/junit/MyJUnit.java
@@ -0,0 +1,73 @@
+package ru.itsjava.annotations.junit;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MyJUnit {
+    private final Object objTestClass;
+    private int passedTests = 0;
+    private int failedTests = 0;
+    private final List<Method> beforeAllMethods = new ArrayList<>();
+    private final List<Method> afterAllMethods = new ArrayList<>();
+    private final List<Method> testMethods = new ArrayList<>();
+    private final List<Method> beforeMethods = new ArrayList<>();
+
+    @SneakyThrows
+    public void start() {
+        fillAllMethodsGroup();
+
+        for (Method method : beforeAllMethods) {
+            method.invoke(objTestClass);
+        }
+
+        for (Method method : testMethods) {
+            for (Method beforeMethod : beforeMethods) {
+                beforeMethod.invoke(objTestClass);
+                System.out.println(beforeMethod.getName());
+            }
+            if (method.isAnnotationPresent(DisplayName.class)){
+                String nameTest = method.getName();
+                System.out.println(nameTest);
+            }
+            try {
+                method.invoke(objTestClass);
+                System.out.println(method.getName() + " PASSED!");
+                passedTests++;
+            } catch (InvocationTargetException exception) {
+                System.out.println(method.getName() + " FAILED!");
+                failedTests++;
+            }
+        }
+        for (Method method : afterAllMethods) {
+            method.invoke(objTestClass);
+        }
+        printResults();
+    }
+
+    private void fillAllMethodsGroup() {
+        for (Method method : objTestClass.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(BeforeAll.class)) {
+                beforeAllMethods.add(method);
+            } else if (method.isAnnotationPresent(Test.class)) {
+                testMethods.add(method);
+            } else if (method.isAnnotationPresent(Before.class)) {
+                beforeMethods.add(method);
+            } else if (method.isAnnotationPresent(AfterAll.class)) {
+                afterAllMethods.add(method);
+            }
+        }
+    }
+
+    private void printResults() {
+        System.out.println("==============================================");
+        System.out.println("Колличество пройденных тестов: " + passedTests);
+        System.out.println("Колличество упавших тестов: " + failedTests);
+        System.out.println("==============================================");
+    }
+}
